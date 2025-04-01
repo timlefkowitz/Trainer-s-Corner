f6b6dc1c566cd045f61e5fea9c84c5d81c57ee71
timothy lefkowitz <timothy.lefkowitz@gmail.com>
2025-03-31 19:54:45 -0500

    Feat: LandingPage

--- trainers_corner_frontend/nft-artwork-tracker/src/components/Home.tsx
+++ trainers_corner_frontend/nft-artwork-tracker/src/components/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import {Box, Button, Typography} from '@mui/material';
 
 const Home = () => (
     <Box sx={{ textAlign: 'center', py: 4 }}>
